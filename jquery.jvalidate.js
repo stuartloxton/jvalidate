4f45aae02eeffee83b44bf014fc76fc28a44e9d1
stuartloxton <stuart.loxton@googlemail.com>
2008-07-20 17:04:50 +0100

    Cleaned up code and added settings

--- jquery.jvalidate.js
+++ jquery.jvalidate.js
@@ -1,3 +1,7 @@
+/*********************************
+ * Settings
+*********************************/
+
 jQuery.validators = {
     url: /^(ht|f)tp(s?)\:\/\/[0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*(:(0-9)*)*(\/?)([a-zA-Z0-9\-\.\?\,\'\/\\\+&amp;%\$#_]*)?$/,
     number: /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/,
@@ -5,6 +9,18 @@ jQuery.validators = {
     email: /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/,
     ip: /\b(([01]?\d?\d|2[0-4]\d|25[0-5])\.){3}([01]?\d?\d|2[0-4]\d|25[0-5])\b/
 }
+jQuery.jValidateSettings = {
+    addClassToParent: true,
+    addClassToForm: true,
+    addClassToInput: true,
+    validClass: 'valid',
+    invalidClass: 'invalid'
+}
+
+/*********************************
+ * Functions
+*********************************/
+
 jQuery.fn.isValid = function() {
     target = this[0];
     if(/input/i.test(target.nodeName)) {
@@ -33,23 +49,36 @@ jQuery.fn.isValid = function() {
         }
     }
 }
+
 jQuery.fn.validate = function() {
     jQuery.each(this, function() {
         if(/form/i.test(this.nodeName)) {
             jQuery(this).find('input').validate();
         } else if(/input/i.test(this.nodeName) && !/submit/i.test(this.type)) {
-            if(jQuery(this).isValid()) {
-                jQuery(this).removeClass('invalid');
-                jQuery(this).addClass('valid');
-            } else {
-                jQuery(this).removeClass('valid');
-                jQuery(this).addClass('invalid');
+            addClass = (jQuery(this).isValid()) ? jQuery.jValidateSettings.validClass : jQuery.jValidateSettings.invalidClass;
+            removeClass = (!jQuery(this).isValie()) ? jQuery.jValidateSettings.validClass : jQuery.jValidateSettings.invalidClass;
+            if(jQuery.jValidateSettings.addClassToInput) {
+                jQuery(this).removeClass(removeClass);
+                jQuery(this).addClass(addClass);
+            }
+            if(jQuery.jValidateSettings.addClassToParent) {
+                jQuery(this).parent().removeClass(removeClass);
+                jQuery(this).parent().addClass(addClass);
+            }
+            if(jQuery.jValidateSettings.addClassToForm) {
+                jQuery(this).parents('form').removeClass(removeClass);
+                jQuery(this).parents('form').addClass(addClass);
             }
         } else if(/select/i.test(this.nodeName)) {
             return true;
         }
     });
 };
+
+/*********************************
+ * Selectors
+*********************************/
+
 $.extend($.expr[':'], {
     valid: "$(a).isValid();",
     invalid: "!$(a).isValid();"
